5e47efeff8b729cb385af02248c9b1b3ceedf363
nidhisharma63 <nidhisharma639593@gmail.com>
2024-04-27 12:14:46 +0530

    research component added

--- src/component/sidebar/TopicComponent.jsx
+++ src/component/sidebar/TopicComponent.jsx
@@ -1,21 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import { useAppContext } from "../../Provider/AppProvider";
+import ResearchData from "../../constant/ResearchData.json";
 
 const TopicComponent = () => {
+  const { setSelectedComponent, selectedComponent } = useAppContext();
+  const handleClickOnResearch = (id) => {
+    const selectedComponent = ResearchData.find((paper) => paper.id === id);
+    setSelectedComponent(selectedComponent);
+  };
   return (
-    <Box sx={{ paddingTop: "15px", paddingLeft: "30px", display: "flex", flexDirection: "column", gap: "20px" }}>
-      TOPIC
-      {/* {UserData.map((user) => {
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      {ResearchData.map((paper) => {
         return (
-          <Box sx={{}}>
-            <Typography color={"#C6C6C7"}> {user.name}</Typography>
+          <Box
+            onClick={() => handleClickOnResearch(paper.id)}
+            sx={{
+              padding: "15px",
+              paddingLeft: "30px",
+              cursor: "pointer",
+              backgroundColor: selectedComponent?.id === paper.id ? "#31333E" : "transparent",
+              "&:hover": {
+                backgroundColor: "#31333E",
+              },
+            }}>
+            <Typography color={"#C6C6C7"}> {paper.name}</Typography>
             <Typography color="#979797" sx={{ marginTop: "5px", fontSize: "12px" }}>
               {" "}
-              Id -{user.id}
+              Id -{paper.id}
             </Typography>
           </Box>
         );
-      })} */}
+      })}
     </Box>
   );
 };
